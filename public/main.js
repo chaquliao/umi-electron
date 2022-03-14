2f32f5648d54aa0408f8cc01919ad687d8742410
廖方正清 <liaofangzhengqing@xiaoduotech.com>
2022-03-14 20:25:34 +0800

    feat: :sparkles: 增加主应用子应用通信

--- public/main.js
+++ public/main.js
@@ -1,21 +1,34 @@
-const { BrowserWindow, app } = require('electron')
+const { BrowserWindow, app, ipcMain } = require('electron')
+const electronReload = require('electron-reload')
 
 const isProd = process.env.NODE_ENV !== 'development'
-
+// console.log(remote)
 let mainWindow;
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: __dirname + '/preload.js'
+    }
   })
 
   if (isProd) {
     mainWindow.loadFile(`${__dirname}/index.html`)
     console.log(`${__dirname}/index.html`)
   } else {
-    mainWindow.loadURL('http://localhost:8000/')
+    mainWindow.loadURL('http://localhost:9691/')
   }
 }
 
 app.on('ready', createWindow)
+
+ipcMain.on('sth', (event, data) => {
+  event.sender.send('sth_response', '主进程的返回1243')
+  console.log(data)
+})
+
+// dialog.showMessageBox({type: 'info', message: '在渲染进程中直接使用主进程的模块'})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './index.css';
+// const ipcRenderer = window.ipcRenderer
+// const ipcRenderer = electron
+const electron = window.require('electron');
+const { ipcRenderer } = electron
 
-export default function() {
+export default function () {
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      console.log(electron);
+    }, 2000)
+
+    ipcRenderer.send('sth', '子进程发送过来的消息')
+
+    ipcRenderer.on('sth_response', (event: any, data: any) => {
+      console.log(new Date())
+      console.log(data)
+    })
+  }, [])
+
+
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
